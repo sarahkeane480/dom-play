c27435bdf95bb10418ed3520d0638d5c9d0f7b67
Sarah Keane <sarahkeane480@gmail.com>
2023-06-28 09:19:29 -0700

    Added comment to dom-play.js to remember requirements

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,1 +1,15 @@
 alert("Hi from DOM play")
+
+/* 
+
+- With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+
+- Un-highlight (normal or otherwise) all lines from all other actors in the play
+
+- The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
+
+- We can change the HTML as needed
+
+*/
+
+
